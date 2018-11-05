378c50568f791789fba2c9b0f17a32c425d28a14
727561126 <727561126@qq.com>
2018-11-05 17:21:27 +0800

    修改为登陆页面

--- polls/views.py
+++ polls/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Question,User
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request,'polls/index.html',context)    
+    return render(request,'polls/login.html')
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
